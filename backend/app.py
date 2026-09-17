f92d2411730d33d91488cdc09e937500df113ef2
Suren-devops <prithivisuren77@gmail.com>
2026-09-17 14:29:38 +0530

    added the chnages

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
+
 from flask import Flask, jsonify
 import os
 
+
 app = Flask(__name__)
+
 
 @app.get("/")
 def home():
@@ -11,13 +14,17 @@ def home():
         "version": os.getenv("APP_VERSION", "local")
     })
 
+
 @app.get("/health")
 def health():
     return jsonify({"status": "healthy"}), 200
+
 
 @app.get("/ready")
 def ready():
     return jsonify({"status": "ready"}), 200
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
